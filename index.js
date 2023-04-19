8cd37ff97eacc1638e1f7c65e108f54c9ec75bb2
Petro Psekho <psekho.pp@gmail.com>
2023-04-19 22:09:18 +0300

    add commander

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const contacts = require('./contacts.js');
 
-// const argv = require('yargs').argv;
+const { program } = require('commander');
+
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
@@ -25,8 +36,9 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
   }
 };
 
+invokeAction(argv);
+
 // invokeAction({ action: 'list' });
 // invokeAction({ action: 'get', id: 'AeHIrLTr6JkxGE6SN-0Rw' });
 // invokeAction({ action: 'add', name: 'Name', email: 'email', phone: 'phone' });
 // invokeAction({ action: 'remove', id: 'AeHIrLTr6JkxGE6SN-0Rw' });
-// invokeAction(argv);
