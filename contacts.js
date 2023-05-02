d2ab0e09c2a7a587f086737c43fdb757c7f48891
Petro Psekho <psekho.pp@gmail.com>
2023-05-02 20:49:05 +0300

    add try…catch to contacts

--- contacts.js
+++ contacts.js
@@ -6,36 +6,56 @@ const contactsPath = path.join(__dirname, 'db/contacts.json');
 console.log(__dirname);
 
 const listContacts = async () => {
-  const data = await fs.readFile(contactsPath);
-  return JSON.parse(data);
+  try {
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to read contacts file');
+  }
 };
 
 const getContactById = async id => {
-  const contacts = await listContacts();
-  const result = contacts.find(item => item.id === id);
-  return result || null;
+  try {
+    const contacts = await listContacts();
+    const result = contacts.find(item => item.id === id);
+    return result || null;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to get contact by id');
+  }
 };
 
 const addContact = async data => {
-  const contacts = await listContacts();
-  const newContact = {
-    id: nanoid(),
-    ...data,
-  };
-  contacts.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return newContact;
+  try {
+    const contacts = await listContacts();
+    const newContact = {
+      id: nanoid(),
+      ...data,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to add contact');
+  }
 };
 
 const removeContact = async id => {
-  const contacts = await listContacts();
-  const idx = contacts.findIndex(item => item.id === id);
-  if (idx === -1) {
-    return null;
+  try {
+    const contacts = await listContacts();
+    const idx = contacts.findIndex(item => item.id === id);
+    if (idx === -1) {
+      return null;
+    }
+    const [result] = contacts.splice(idx, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to remove contact');
   }
-  const [result] = contacts.splice(idx, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return result;
 };
 
 module.exports = {
